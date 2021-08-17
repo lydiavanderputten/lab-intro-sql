b0fd05d53daa8710a5bca13f14e6bea8fadc93aa
lydiavanderputten <84539257+lydiavanderputten@users.noreply.github.com>
2021-08-17 21:53:21 +0200

    Add files via upload

---
+++ Lab intro SQL.sql
@@ -0,0 +1,17 @@
+USE Sakila;
+SELECT database();
+/**Review the tables in the database --> I used the information button*/
+-- Explore tables by selecting all columns from each table or using the in built review features for your client*/
+SHOW FULL TABLES;
+SELECT title FROM film;
+SELECT name as language from language;
+
+
+
+
+
+
+
+
+
+
